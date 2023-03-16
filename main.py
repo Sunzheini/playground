5c330671685b0f87b3830ff450af1372545ec388
Sunzheini <daniel_zorov@abv.bg>
2023-03-16 15:01:12 +0200

    pyautogui

--- main.py
+++ main.py
@@ -1,137 +1,31 @@
-from tkinter import *
-from PIL import ImageTk
-import sqlite3
+"""
+200 – OK. The request was successful. The answer itself depends on the method 
+used (GET, POST, etc.) and the API specification.
+204 – No Content. The server successfully processed the request and did not 
+return any content.
+301 – Moved Permanently. The server responds that the requested page (endpoint) 
+has been moved to another address and redirects to this address.
+400 – Bad Request. The server cannot process the request because the client-side 
+errors (incorrect request format).
+401 – Unauthorized. Occurs when authentication was failed, due to incorrect 
+credentials or even their absence.
+403 – Forbidden. Access to the specified resource is denied.
+404 – Not Found. The requested resource was not found on the server.
+500 – Internal Server Error. Occurs when an unknown error has occurred on the server.
 
+"""
+import requests
 
-def submit():
-    conn = sqlite3.connect("daniel_app.db")
-    c = conn.cursor()
+url = "https://google-translate1.p.rapidapi.com/language/translate/v2/detect"
 
-    # submit
-    c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :zipcode)",
-              {
-                  'f_name': f_name.get(),
-                  'l_name': l_name.get(),
-                  'address': address.get(),
-                  'zipcode': zipcode.get(),
-              })
+payload = "q=English%20is%20hard%2C%20but%20detectably%20so"
+headers = {
+	"content-type": "application/x-www-form-urlencoded",
+	"Accept-Encoding": "application/gzip",
+	"X-RapidAPI-Key": "9eee0d615fmsh414ace264454d8ap14a435jsn486edea4b81e",
+	"X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
+}
 
-    conn.commit()
-    conn.close()
+response = requests.request("POST", url, data=payload, headers=headers)
 
-    # clear the textbox
-    f_name.delete(0, END)
-    l_name.delete(0, END)
-    address.delete(0, END)
-    zipcode.delete(0, END)
-
-
-def query():
-    conn = sqlite3.connect("daniel_app.db")
-    c = conn.cursor()
-
-    # query
-    c.execute("SELECT *, oid FROM addresses")
-    records = c.fetchall()
-
-    print_records = ''
-    for r in records:            # records[0] is first line from database
-        print_records += str(r) + '\n'
-
-    query_label = Label(root, text=print_records)
-    query_label.grid(row=8, column=0, columnspan=2)
-
-    conn.commit()
-    conn.close()
-
-
-def delete():
-    conn = sqlite3.connect("daniel_app.db")
-    c = conn.cursor()
-
-    # delete
-    c.execute("DELETE FROM addresses WHERE oid = " + delete_box.get())
-
-    conn.commit()
-    conn.close()
-
-
-def edit():
-    conn = sqlite3.connect("daniel_app.db")
-    c = conn.cursor()
-
-    # edit
-    c.execute("DELETE FROM addresses WHERE oid = " + delete_box.get())
-
-    conn.commit()
-    conn.close()
-
-
-root = Tk()
-root.title("Daniel's APP")
-root.eval("tk::PlaceWindow . center")       # place windows in the center
-
-x = root.winfo_screenwidth() // 2           # 3: left edge starts from, 1/3 of screen width
-y = int(root.winfo_screenheight() * 0.2)    # top edge starts from 20% of screen height
-root.geometry('500x600+' + str(x) + '+' + str(y))
-
-
-# create db
-conn = sqlite3.connect("daniel_app.db")
-c = conn.cursor()
-
-# create table - run 1 time only - needs to be uncommented also when running pyinstaller
-c.execute("""
-    CREATE TABLE IF NOT EXISTS addresses(
-    first_name text,
-    last_name text,
-    address text,
-    zipcode integer
-    )
-""")
-
-
-# labels
-f_name_label = Label(root, text="first name")
-f_name_label.grid(row=0, column=0)
-f_name = Entry(root, width=30)
-f_name.grid(row=0, column=1, padx=20)
-
-l_name_label = Label(root, text="last name")
-l_name_label.grid(row=1, column=0)
-l_name = Entry(root, width=30)
-l_name.grid(row=1, column=1, padx=20)
-
-address_label = Label(root, text="address")
-address_label.grid(row=2, column=0)
-address = Entry(root, width=30)
-address.grid(row=2, column=1, padx=20)
-
-zipcode = Label(root, text="zipcode")
-zipcode.grid(row=3, column=0)
-zipcode = Entry(root, width=30)
-zipcode.grid(row=3, column=1, padx=20)
-
-delete_box = Label(root, text="delete")
-delete_box.grid(row=9, column=0)
-delete_box = Entry(root, width=30)
-delete_box.grid(row=9, column=1, padx=20)
-
-
-# buttons
-submit_bt = Button(root, text="Add Record To Database", command=submit)
-submit_bt.grid(row=4, column=0, columnspan=2, pady=10, padx=10)
-
-query_bt = Button(root, text="Get Record From Database", command=query)
-query_bt.grid(row=6, column=0, columnspan=2, pady=10, padx=10)
-
-query_bt = Button(root, text="Delete", command=delete)
-query_bt.grid(row=10, column=0, columnspan=2, pady=10, padx=10)
-
-
-# commit changes and close connection
-conn.commit()
-conn.close()
-
-# -----------------------------------------------------------------------
-root.mainloop()
+print(response.text)
