64f776dc0ad0eff2fefe04f013a174a285795c1b
Sunzheini <daniel_zorov@abv.bg>
2025-08-18 13:14:26 +0300

    expanded vbox exercise

--- lab/exercise_vbox_mng.py
+++ lab/exercise_vbox_mng.py
@@ -1,4 +1,23 @@
 import subprocess
 
-path = 'C:\\Program Files\\Notepad++\\notepad++.exe'
-subprocess.Popen(path)
+path: str = 'C:\\Program Files\\Notepad++\\notepad++.exe'
+
+
+def open_path(path_to_open: str) -> None:
+    subprocess.Popen(path_to_open)
+
+
+def main() -> None:
+    try:
+        open_path(path)
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print("This script is not being run directly.")
+    print("It is likely being imported as a module.")
