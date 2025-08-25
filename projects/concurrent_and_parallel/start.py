a203c83324aa3df1cb2630590f35df8d469a361b
Sunzheini <daniel_zorov@abv.bg>
2025-08-25 18:03:06 +0300

    Added a comment

--- projects/concurrent_and_parallel/start.py
+++ projects/concurrent_and_parallel/start.py
@@ -1,6 +1,17 @@
-def run_program():
-    pass
+def run_program(*args, **kwargs):
+    if args:
+        print(f"args: {', '.join(str(arg) for arg in args)}")
+    if kwargs:
+        print(f"kwargs: {kwargs}")
+
+
+"""
+Continue with:
+https://realpython.com/async-io-python/
+Udemy course
+Deepseek conversation
+"""
 
 
 if __name__ == "__main__":
-    run_program()
+    run_program(4, 5)
