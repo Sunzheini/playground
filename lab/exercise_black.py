2f9ea8dfa4bdb23862e8a984221c300f7f35c345
Sunzheini <daniel_zorov@abv.bg>
2025-10-13 11:03:15 +0300

    Add black configuration and update requirements.txt with black and additional dependencies

---
+++ lab/exercise_black.py
@@ -0,0 +1,3 @@
+# black --check .          # See what would change
+# black --diff .           # Preview changes
+# black .                  # Apply changes, but exclude venv, .venv, .idea, migrations, test, __pycache__
