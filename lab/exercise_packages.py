5f4d517def80c09ad788f9bde08f6d0ce5a7d2b7
Sunzheini <daniel_zorov@abv.bg>
2025-10-23 12:42:56 +0300

    Add setup instructions and usage guidelines for shared-lib package

---
+++ lab/exercise_packages.py
@@ -0,0 +1,40 @@
+"""
+1. create a folder: `D:\Study\Projects\Github\AegisAI\shared-lib`
+2. `poetry init` -> creates a pyproject.toml
+3. inside create a folder `shared_lib` with __init__.py inside
+4. change toml to:
+    [project]
+    name = "shared-lib"
+    version = "0.1.0"
+    description = "Shared code for microservices"
+    authors = [
+        {name = "Sunzheini",email = "daniel_zorov@abv.bg"}
+    ]
+    readme = "README.md"
+    requires-python = ">=3.11"
+    dependencies = [
+    ]
+
+    [tool.setuptools]
+    packages = ["shared_lib"]
+
+    [build-system]
+    requires = ["poetry-core>=2.0.0,<3.0.0"]
+    build-backend = "poetry.core.masonry.api"
+5. moved shared files there
+6. in services/api-gateway-service/pyproject.toml add:
+`"shared-lib @ file:///D:/Study/Projects/Github/AegisAI/shared-lib"`
+at the end of the `dependencies` list
+7. in services/api-gateway-service/ run:
+`poetry lock` and `poetry install --no-root`,
+you should see `  - Installing shared-lib (0.1.0 D:/Study/Projects/Github/AegisAI/shared-lib)`
+8. change from
+`contracts.job_schemas import IngestionJobRequest` to
+`shared_lib.contracts.job_schemas import IngestionJobRequest`
+"""
+
+"""
+Updating the shared-lib package:
+1. (optional) version = "0.1.1" in shared-lib/pyproject.toml
+2. in services/api-gateway-service/ run:`poetry update shared-lib`
+"""
