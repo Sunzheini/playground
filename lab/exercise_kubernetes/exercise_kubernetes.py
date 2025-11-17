0a71a6e21f9b860ebb0f9251db5d7f46e081d3bf
Sunzheini <daniel_zorov@abv.bg>
2025-11-17 11:40:17 +0200

    Add Kubernetes deployment and service configuration files

--- lab/exercise_kubernetes/exercise_kubernetes.py
+++ lab/exercise_kubernetes/exercise_kubernetes.py
@@ -1,11 +1,8 @@
-
-
-
-# kubectl commands to create a deployment and expose it as a service
+# kubectl commands
 """
-
-
-
-
-
+kubectl create -f .\nginx-pod-definition.yml        # create a pod from the definition file
+kubectl get pods                                    # list all pods
+kubectl describe pod myapp-pod                      # describe the pod in detail
+kubectl logs myapp-pod                              # get the logs from the pod
+kubectl delete pod myapp-pod                        # delete the pod
 """
