d7b689aea38757a868451902c9848c41bb24608d
Sunzheini <daniel_zorov@abv.bg>
2025-11-17 13:00:16 +0200

    Enhance exercise_kubernetes.py with additional kubectl commands and pod access instructions

--- lab/exercise_kubernetes/exercise_kubernetes.py
+++ lab/exercise_kubernetes/exercise_kubernetes.py
@@ -1,8 +1,14 @@
 # kubectl commands
 """
-kubectl create -f .\nginx-pod-definition.yml        # create a pod from the definition file
-kubectl get pods                                    # list all pods
-kubectl describe pod myapp-pod                      # describe the pod in detail
-kubectl logs myapp-pod                              # get the logs from the pod
-kubectl delete pod myapp-pod                        # delete the pod
+kubectl create -f .\nginx-pod-definition.yml            # create a pod from the definition file
+kubectl get pods                                        # list all pods
+kubectl get pods -o wide                                # list all pods with more details
+kubectl describe pod myapp-pod                          # describe the pod in detail
+kubectl logs myapp-pod                                  # get the logs from the pod
+kubectl delete pod myapp-pod                            # delete the pod
+
+kubectl exec -it myapp-pod -- /bin/bash                 # Access pod's terminal
+curl localhost:8000                                     # Check if nginx serves on port 8000
+
+kubectl run my-nginx --image=nginx:latest --port=80     # create and run a pod with nginx image
 """
