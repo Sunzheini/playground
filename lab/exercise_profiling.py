851c40c16884e7a91272d021c1086d3a0615a741
Sunzheini <daniel_zorov@abv.bg>
2026-03-02 13:37:27 +0200

    Enable function profiling in exercise_profiling.py with cProfile and pstats

--- lab/exercise_profiling.py
+++ lab/exercise_profiling.py
@@ -12,13 +12,13 @@ def sample_function():
 
 
 # ------------------------- profile a function -----------------------------------------
-# cProfile.run('sample_function()', 'profiling/output.prof')
-#
-# # Read and sort the stats
-# p = pstats.Stats('profiling/output.prof')
-# p.strip_dirs().sort_stats('cumulative').print_stats(20)  # Top 20 by cumulative time
-# p.sort_stats('time').print_stats(20)  # Top 20 by internal time
-# p.sort_stats('calls').print_stats(20)  # Top 20 by number of calls
+cProfile.run('sample_function()', 'profiling/output.prof')
+
+# Read and sort the stats
+p = pstats.Stats('profiling/output.prof')
+p.strip_dirs().sort_stats('cumulative').print_stats(20)  # Top 20 by cumulative time
+p.sort_stats('time').print_stats(20)  # Top 20 by internal time
+p.sort_stats('calls').print_stats(20)  # Top 20 by number of calls
 
 
 # ------------------------- memory profiling (requires memory_profiler package) --------
